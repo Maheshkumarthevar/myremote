565650908353def892940a976bbfdf377fa56064
Mahesh Kumar <maheshkumar10042005@gmail.com>
2026-03-07 15:42:36 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,1 +1,9 @@
-
+import java.util.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        HashSet<String> city= new HashSet<>();
+        System.out.println("whether the city object is empty or not by True/False : "+city.isEmpty());
+       city.add("Tokyo");
+       city.add("Toronto");
+        city.add("London");
